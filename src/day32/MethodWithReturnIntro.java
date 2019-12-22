6a5d974d0a9b5c78389609d1ab0f8b122a95bf4d
Yerkhanat <erha.mts@gmail.com>
2019-12-22 13:47:55 -0800

    method call Array

---
+++ src/day32/MethodWithReturnIntro.java
@@ -0,0 +1,15 @@
+package day32;
+
+public class MethodWithReturnIntro {
+    public static void main(String[] args) {
+   String name = giveMeMyName();
+        System.out.println("name = " + name);
+    }public static String giveMeMyName(){
+
+       // assume getting my name is multi step complex operation
+
+        return "Yerkhanat";
+
+
+    }
+}
