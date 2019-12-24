5877ade64db361b62bdbe92284172d56c2a9c940
Yerkhanat <erha.mts@gmail.com>
2019-12-23 18:32:12 -0800

    method call Array

--- src/day32/MethodWithReturnIntro.java
+++ src/day32/MethodWithReturnIntro.java
@@ -5,6 +5,9 @@ public class MethodWithReturnIntro {
    String name = giveMeMyName();
         System.out.println("name = " + name);
         System.out.println("doubled number: "+doubleTheNumber(100));
+        System.out.println(divide(10,3));
+        System.out.println(divide(10,0));
+        System.out.println(divide(0,10));
 
         // create a static method that double the value of a number
     }public static String giveMeMyName(){
@@ -15,5 +18,13 @@ public class MethodWithReturnIntro {
     }public static int doubleTheNumber(int number){
         System.out.println("I am going to double the number "+number);
         return number*2;
+    }
+    public static double divide(double num1, double num2){
+      if(num2==0){
+          return 0.0;
+      }else {
+          return num1/num2;
+      }
+
     }
 }
