47c0deb4d5265228d2ded292329b4fa09293b643
Yerkhanat <erha.mts@gmail.com>
2020-01-05 09:16:03 -0800

    Day 35 - Character Practice

--- src/day35/CharPractice2.java
+++ src/day35/CharPractice2.java
@@ -2,6 +2,16 @@ package day35;
 
 public class CharPractice2 {
     public static void main(String[] args) {
+     String str = "A34B123C4X";
+     int sum =0;
+     char [] allChar = str.toCharArray();
+     for (char each: allChar){
+         if(Character.isDigit(each)){
+             sum += Integer.parseInt(each+"");
 
+         }
+
+     }
+        System.out.println(sum);
     }
 }
